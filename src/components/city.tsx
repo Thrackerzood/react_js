d922a932f45a0e75944cab4e0090aa969a885240
Thrackerzood <71223383+Thrackerzood@users.noreply.github.com>
2021-08-20 22:39:22 +0400

    Add files via upload

---
+++ src/components/city.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { ajax } from "../middleware/ajax";
+import { CityS } from "../style/style";
+
+
+
+
+function City(props:{props: string}):JSX.Element {
+   const [state, fetchLoad]= useState({data: {message: [{name_city: ""}]}})
+   const result = async () => {
+     let response:any = await ajax(`https://sql-basedata.herokuapp.com/api/allCountry/${props.props}`)
+     fetchLoad({data: response})
+   }
+   useEffect(()=>{ result() }, [])
+   return (<ul>
+        {state.data.message.map((element:any, index:number):JSX.Element => {
+           return <CityS key={index}>
+                     <p>{element.name_city}</p>
+                  </CityS>
+                  
+        })} 
+   </ul>);
+ }
+ 
+ export default City;
---
+++ src/components/citySearchMap.tsx
@@ -0,0 +1,126 @@
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { CityS } from "../style/style";
+import { IResponse } from "./cityPage";
+import {getGeocode, getLatLng } from "use-places-autocomplete";
+ import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+
+
+
+
+function CitySearchMap(props:{props:IResponse}):JSX.Element {
+   const [state, fetchLoad]
+   :[IResponse,React.Dispatch<React.SetStateAction<IResponse>>] 
+   = useState({data: {message: [{name_city: ""}]}})
+   const [currentPage, setCurrentPage]
+   :[number,React.Dispatch<React.SetStateAction<number>>] 
+   = useState(1)
+   const [postsPerPage]
+   :[number, Dispatch<SetStateAction<number>>] 
+   = useState(5)
+   // логика смены страницы
+   const totalPage:number[] = []
+   const indexOfLastPost:number = currentPage * postsPerPage
+   const indexOfFirstPost:number = indexOfLastPost - postsPerPage
+   const setCurrentPost:{name_city: string;}[] = state.data.message.slice(indexOfFirstPost, indexOfLastPost)
+   for(let i = 1; i <= Math.ceil(state.data.message.length / postsPerPage); ++i){
+      totalPage.push(i)
+   }
+   //смена страницы
+   const previewPage = ():void =>{
+      if(currentPage <= 1){
+         setCurrentPage(previous => previous = 1)
+      
+      }else{
+         setCurrentPage(previous => previous - 1)
+      }
+   }
+   //следующая страница
+   const nextPage
+   :() => void = ():void => {
+      if(currentPage >= totalPage.length){
+         setCurrentPage(previous => previous = totalPage.length)
+      }else{
+         setCurrentPage(previous => previous + 1)
+      }
+   }
+   //смена страницы по выбору кнопки
+   const paginate
+   :(numberPage: number) => void 
+   = (numberPage:number):void => {
+      setCurrentPage(numberPage)
+   }
+   useEffect(() => {
+      const result 
+      :() => Promise<void> 
+      = async () => {
+         let response:any = await props.props.data
+         await fetchLoad({data: response})
+      }
+      result()
+   }, [props.props.data, currentPage])
+   
+   const {isLoaded, loadError} = useLoadScript({
+      googleMapsApiKey: 'AIzaSyBdc0x4iD0GPspP6dSvw37ZadVsMKg8k8o',
+      libraries: ['places'],
+
+   })
+   const mapRef:any = React.useRef();
+   const onMapLoad:any = React.useCallback((map:any) => {
+     mapRef.current = map;
+   }, []);
+   // передает обратно при смене города
+   const panTo:({ lat, lng }: any) => void = React.useCallback(({ lat, lng }) => {
+      mapRef.current.panTo({ lat, lng });
+      mapRef.current.setZoom(14);
+    }, []);
+   //проверка на ошибки
+   if(loadError) return <p>'Ошибка загрузки карты'</p>
+   //проверка на загрузку карты
+   if(!isLoaded) return <p>'Загрузка карты'</p>
+   //устанавление стиля карты, так же можно менять цвета
+   const mapContainerStyle = {
+      width: '100%',
+      minHeight: '50vh'
+   }
+   //стандартная позиция карты при входе
+   const center ={
+      lat: 56.8637312,
+      lng: 53.088019
+   }
+return (<><ul>
+         {setCurrentPost.map((element:any, index:number):JSX.Element => {
+            return   <CityS key={index}>
+                        <p className="searchP" onClick={async(event:any) => {
+                           let address = await event.target.textContent
+                           console.log(address)
+                           const results = await getGeocode({address})
+                           const {lat , lng} = await getLatLng(results[0])
+                           panTo({lat , lng})
+                           }}>{element.name_city}</p>
+                     </CityS>
+                  })}
+            <GoogleMap
+            //сама карта
+            mapContainerStyle={mapContainerStyle}
+            //стандартный скейл при входе
+            zoom={8}
+            center={center}
+            //меняет позицию карты при смене lat / lng
+            onLoad={onMapLoad}
+            ></GoogleMap>
+            
+         <div className="page">
+            
+         {totalPage.length === 1 ? <></> : <button onClick={() => previewPage()}> назад </button>}
+         {totalPage.length === 1 ? <></> : totalPage.map((number:number):JSX.Element => {
+            return <button onClick={() => paginate(number)} key={number}>{number}</button>
+         })
+         }
+         {totalPage.length === 1 ? <></> : <button onClick={() => nextPage()}> вперёд </button>}
+         </div>
+         </ul>
+         </>
+      )
+ }
+ 
+ export default CitySearchMap;
